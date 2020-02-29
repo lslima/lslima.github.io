3dc7d73a8b269e607517dc37851cf79c7065ff38
Leonardo <maildoleo@gmail.com>
2020-02-29 16:49:46 -0300

    novo bg

--- aulas/js/sketch.js
+++ aulas/js/sketch.js
@@ -1,15 +1,60 @@
+var circulosos = [];
+
 function setup(){
   createCanvas(windowWidth, windowHeight);
-  noLoop();
+
+  for (var i = 0; i < floor(random(5, 20)); i++) {
+    var ran1 = floor(random(width));
+    var ranc = floor(random(255));
+    circulosos[i] = new Circuleta(ran1, 0, ran1, ranc);
+  }
+  setInterval(targetGen, 4000);
 }
+
 function draw(){
   background(204);
-  circlesL(800,0);
+  for (var i = 0; i < circulosos.length; i++) {
+    circulosos[i].atualiza();
+    circulosos[i].display();
+  }
 }
-function circlesL(x, y){
-	for(let i = 0; i <=3; i++){
+
+function targetGen() {
+  for (var i = 0; i < circulosos.length; i++) {
+    circulosos[i].randomizer();
+  }
+}
+
+function Circuleta(tempX, tempY, tempDiameter, tempColor) {
+  this.x = tempX;
+  this.y = tempY;
+  this.d = tempDiameter;
+  this.c = tempColor;
+  this.easing = 0.05;
+  this.targetX = floor(random(width));
+  this.targetD = floor(random(height));
+  this.targetC = floor(random(255));
+
+  this.randomizer = function(){
+    this.targetX = floor(random(width));
+    this.targetD = floor(random(width));
+    this.targetC = floor(random(255));
+  }
+
+  this.atualiza = function(){
+    this.dx = this.targetX - this.x;
+    this.x += this.dx * this.easing;
+
+    this.dd = this.targetD - this.d;
+    this.d += this.dd * this.easing;
+
+    this.dc = this.targetC - this.c;
+    this.c += this.dc * this.easing;
+  }
+
+  this.display = function(){
     noFill();
-		stroke( random (["gray", "black", "white"]));
-		ellipse(random(0, x), y, random(000, displayWidth));
-	}
+    stroke(this.c);
+    ellipse(this.x, this.y, this.d, this.d);
+  }
 }
